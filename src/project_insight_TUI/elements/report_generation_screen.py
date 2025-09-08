c06019c58ba4c5e17a92df7d961a2a9af6502040
RajHarsora <harsora@rowan.edu>
2025-09-08 06:02:48 -0400

    created report_generation page

--- src/project_insight_TUI/elements/report_generation_screen.py
+++ src/project_insight_TUI/elements/report_generation_screen.py
@@ -1,17 +1,30 @@
 from textual.app import ComposeResult
 from textual.screen import Screen
-from textual.containers import HorizontalGroup
-from textual.widgets import Label, Button, Footer, Header
+from textual.containers import HorizontalGroup, VerticalGroup
+from textual.widgets import Label, Button, Footer, Header, Input
 from ..elements.menu_screen import MenuScreen
-
+from tkinter import filedialog
 class ReportGenerationScreen(Screen):
     CSS_PATH = "report_generation_screen.tcss"
+    
+    def __init__(self):
+        super().__init__()
+        self.save_location = None
 
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
-        yield Label("1. Choose a date and save location \n 2. Generate the report!", id = "report_generation_message")
+        yield Label("Report Generation Page", id="title_label")
+        yield VerticalGroup(
+            Label("1. Choose a date and save location", id = "instruction_label1"),
+            Label("2. Generate the report!", id = "instruction_label2"),
+            id="instructions_container"
+        )
+        yield Input(placeholder="Enter Date (YYYY-MM-DD)", id="date_input")
+        yield Button("Choose Save Location", id="save_location_button")
+        yield Label("", id="status_label")
         yield HorizontalGroup(
             Button(label="Back to Main Menu", id="back_to_main_menu_button"),
+            Button(label="Generate Report", id="generate_report_button"),
             Button(label="Exit Application", id="exit_button"),
             id="action_buttons"
         )
@@ -22,3 +35,8 @@ class ReportGenerationScreen(Screen):
             self.app.exit()
         elif event.button.id == "back_to_main_menu_button":
             self.app.push_screen(MenuScreen())
+        elif event.button.id == "save_location_button":
+            path = filedialog.askdirectory(title="Select Report Save Location")
+            if path:
+                self.save_location = path
+                self.query_one("#status_label", Label).update(f"Save location set to: {self.save_location}")
